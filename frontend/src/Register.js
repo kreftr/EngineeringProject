5a95f73eb521b3b026fc4da8c675ab9256480856
k0n0di0d4 <k00n00di00d4@gmail.com>
2021-11-05 10:42:37 +0100

    Next stage of frontend development

---
+++ frontend/src/Register.js
@@ -0,0 +1,56 @@
+import './Register.css';
+import React, {Component} from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+class Register extends Component {
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.registerUser(event.target.username.value, event.target.password.value);
+    }
+
+    registerUser(username, password) {
+        fetch('http://localhost:8080/users', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            })
+        }).then(function(response) {
+            if(response.status === 200) {
+                console.log("User registered!");
+            } else if (response.status === 422) {
+                console.log("User already exists");
+            }
+            else {
+                console.log("User not registered");
+            }
+        }).catch(function(error){
+            console.log("error");
+        });
+    }
+
+    render() {
+        return <div className="Register">
+            <Form onSubmit = {this.handleSubmit}>
+                <Form.Group controlId="username" size="lg">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control autoFocus name="username"/>
+                </Form.Group>
+
+                <Form.Group controlId="password" size="lg">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name="password"/>
+                </Form.Group>
+
+                <Button block size = "lg" type = "submit">Register</Button>
+            </Form>
+        </div>
+    }
+}
+
+export default Register;
