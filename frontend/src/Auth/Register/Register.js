797ea23399552a6deda2929418c11ca165958ddb
Filip Magdziak <k00n00di00d4@gmail.com>
2021-11-07 18:10:46 +0100

    First login and Register (not working yet) Cleanup in the Frontend folder

--- frontend/src/Auth/Register/Register.js
+++ frontend/src/Auth/Register/Register.js
@@ -2,15 +2,33 @@ import './Register.css';
 import React, {Component} from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import RegisterAlert from "./RegisterAlert.js";
+
 class Register extends Component {
+
+    constructor(props) {
+        super(props);
+        this.registerAlert = React.createRef();
+    }
+
+    showRegisterAlert(variant, heading, message) {
+        this.registerAlert.current.setVariant(variant)
+        this.registerAlert.current.setHeading(heading)
+        this.registerAlert.current.setMessage(message)
+        this.registerAlert.current.setVisible(true)
+    }
 
     handleSubmit = event => {
         event.preventDefault();
-        this.registerUser(event.target.username.value, event.target.password.value);
+        this.registerUser(
+            event.target.username.value,
+            //event.target.email.value,
+            event.target.password.value
+        );
     }
 
     registerUser(username, password) {
-        fetch('http://localhost:8080/users', {
+        fetch('http://localhost:8080/registration', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
@@ -21,8 +39,8 @@ class Register extends Component {
                 password: password,
             })
         }).then(function(response) {
-            if(response.status === 200) {
-                console.log("User registered!");
+            if(response.status === 201) {
+                this.showRegisterAlert("success", "User created", response.message);
             } else if (response.status === 422) {
                 console.log("User already exists");
             }
