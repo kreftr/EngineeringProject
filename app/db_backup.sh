b2e4965df146da42f52e13a7b39724f3345c45bf
nawrotadam <nawrotadam02@gmail.com>
2022-05-26 00:34:46 -0700

    Database backup bugs fixed

--- app/db_backup.sh
+++ app/db_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# $1 <- docker container
-# $2 <- path to database backup folder
+# $1 <- path to database backup folder
 
+docker_container=$(docker ps -f "name=app_db_1" --format '{{.ID}}')
 
-docker exec -t "$1" pg_dumpall -c -U admin > "$2"//dump_"$(date +%d-%m-%Y"_"%H_%M_%S)".sql
+docker exec -t "${docker_container}" pg_dumpall -c -U admin > "$1"//dump_"$(date +%d-%m-%Y"_"%H_%M_%S)".sql
