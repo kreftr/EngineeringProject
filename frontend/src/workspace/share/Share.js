3fd89b79de2906de4bf3092c62564150bdc9a85e
nawrotadam <nawrotadam02@gmail.com>
2022-06-01 19:03:57 +0200

    Share section created

---
+++ frontend/src/workspace/share/Share.js
@@ -0,0 +1,15 @@
+import {Button, Col, Image, Row} from "react-bootstrap";
+import React, {useEffect, useState} from "react";
+
+function Share(props){
+
+    return (
+        <>
+            <center>
+                <p>Test</p>
+            </center>
+        </>
+    );
+
+}
+export default Share;
