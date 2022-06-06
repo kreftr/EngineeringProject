fc5ffbefc42692b440ab8b82d71205fe1a8020c5
nawrotadam <nawrotadam02@gmail.com>
2022-06-07 01:55:06 +0200

    Basic spring boot logging overrided with Log4j2, it still logs too much to terminal

--- app/src/main/java/edu/pjatk/app/WebApp.java
+++ app/src/main/java/edu/pjatk/app/WebApp.java
@@ -2,12 +2,18 @@ package edu.pjatk.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
 public class WebApp {
 
+    private static final Logger LOGGER = LogManager.getLogger(WebApp.class);
+
     public static void main(String[] args) {
         SpringApplication.run(WebApp.class, args);
+
+        LOGGER.info("Info level log message");
+        LOGGER.error("Error level log message");
     }
 }
