617fde80d426abb3e10ba8c4f06b604c035f39d1
Filip Magdziak <k00n00di00d4@gmail.com>
2022-06-20 22:23:14 +0200

    Fix

--- app/src/main/java/edu/pjatk/app/request/PostRequest.java
+++ app/src/main/java/edu/pjatk/app/request/PostRequest.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.Size;
 @Getter
 @Setter
 public class PostRequest {
-    @Size(min = 3, max = 24, message = "{forum.post.username.size}")
+    @Size(min = 3, max = 24, message = "{forum.post.title.size}")
     private String title;
     private String text;
 }
